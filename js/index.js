baa1b169db66a231eeec6c8fd6ae92150fe34b2a
SabLuv <124561798+SabLuv@users.noreply.github.com>
2024-05-04 16:14:30 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let countNode = document.querySelector(`#count`);
+let clickNode = document.querySelector(`#click`);
+let decorNode = document.querySelector(`#decor`);
+
+
+let count = 80;
+
+clickNode.addEventListener(`click`, function() {
+    count++;
+
+    countNode.innerHTML = count;
+
+    if (count == 100) {
+        decorNode.style.display = `block`;
+        clickNode.style.marginTop = `0px`;
+    }
+})
